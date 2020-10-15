028db355d878cc650d3186a0bcfbc4b705bbd630
FUWEN TAN <fuwen.tan@gmail.com>
2020-10-15 16:17:39 -0400

    gldv2 test

---
+++ research/delf/delf/python/delg/gldv2_extraction_gldv2.sh
@@ -0,0 +1,17 @@
+# python extract_gldv2_features.py \
+#   --delf_config_path r50delg_gldv2clean_config.pbtxt \
+#   --dataset_file_path data/gldv2/train_local.txt \
+#   --images_dir data/gldv2/ \
+#   --output_features_dir data/gldv2/delg_features
+
+python extract_gldv2_features.py \
+  --delf_config_path r50delg_gldv2clean_config.pbtxt \
+  --dataset_file_path data/gldv2/test_query.txt \
+  --images_dir data/gldv2/ \
+  --output_features_dir data/gldv2/delg_features_query
+
+python extract_gldv2_features.py \
+  --delf_config_path r50delg_gldv2clean_config.pbtxt \
+  --dataset_file_path data/gldv2/test_gallery.txt \
+  --images_dir data/gldv2/ \
+  --output_features_dir data/gldv2/delg_features_index
