efa14573e0de7ed8de3c080613d5f2c5f747a0b5
FUWEN TAN <fuwen.tan@gmail.com>
2020-10-29 22:07:35 -0400

    revisited1m

---
+++ research/delf/delf/python/delg/d1m_extraction_gldv1.sh
@@ -0,0 +1,5 @@
+python extract_d1m_features.py \
+  --delf_config_path r50delg_gld_config.pbtxt \
+  --dataset_file_path data/revisitop1m/revisitop1m.txt \
+  --images_dir data/revisitop1m/jpg \
+  --output_features_dir data/revisitop1m/delg_gldv1
