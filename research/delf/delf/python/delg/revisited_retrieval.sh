cfe244577eea3843f83d485f6502e210e2f08796
FUWEN TAN <fuwen.tan@gmail.com>
2020-11-06 13:27:18 -0500

    path

--- research/delf/delf/python/delg/revisited_retrieval.sh
+++ research/delf/delf/python/delg/revisited_retrieval.sh
@@ -1,27 +1,13 @@
 python perform_retrieval.py \
-  --dataset_file_path data/oxford5k/gnd_roxford5k.mat \
-  --query_features_dir data/oxford5k/delg_features_r50 \
-  --index_features_dir data/oxford5k/delg_features_r50 \
+  --dataset_file_path data/revisitop1m/gnd_roxford5k.mat \
+  --query_features_dir data/revisitop1m/delg_features_r50 \
+  --index_features_dir data/revisitop1m/delg_features_r50 \
   --use_geometric_verification \
   --output_dir results/oxford5k_r50_with_gv
 
 python perform_retrieval.py \
-  --dataset_file_path  data/paris6k/gnd_rparis6k.mat \
-  --query_features_dir data/paris6k/delg_features_r50 \
-  --index_features_dir data/paris6k/delg_features_r50 \
+  --dataset_file_path  data/revisitop1m/gnd_rparis6k.mat \
+  --query_features_dir data/revisitop1m/delg_features_r50 \
+  --index_features_dir data/revisitop1m/delg_features_r50 \
   --use_geometric_verification \
   --output_dir results/paris6k_r50_with_gv
-
-# python perform_retrieval.py \
-#   --dataset_file_path data/oxford5k/gnd_roxford5k.mat \
-#   --query_features_dir data/oxford5k/delg_features_r101/query \
-#   --index_features_dir data/oxford5k/delg_features_r101/index \
-#   --use_geometric_verification \
-#   --output_dir results/oxford5k_r101_with_gv
-
-# python perform_retrieval.py \
-#   --dataset_file_path  data/paris6k/gnd_rparis6k.mat \
-#   --query_features_dir data/paris6k/delg_features_r101/query \
-#   --index_features_dir data/paris6k/delg_features_r101/index \
-#   --use_geometric_verification \
-#   --output_dir results/paris6k_r101_with_gv
