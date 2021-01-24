67b575eb90c2837cf2c114759468a91143639081
FUWEN TAN <fuwen.tan@gmail.com>
2021-01-23 23:45:24 -0500

    local

---
+++ research/delf/delf/python/detect_to_retrieve/local_retrieval.sh
@@ -0,0 +1,9 @@
+# From models/research/delf/delf/python/detect_to_retrieve
+python perform_retrieval_delg_local.py \
+  --index_aggregation_config_path index_aggregation_config_delg.pbtxt \
+  --query_aggregation_config_path query_aggregation_config_delg.pbtxt \
+  --dataset_file_path  data/paris6k/gnd_rparis6k.mat \
+  --index_aggregation_dir data/paris6k_aggregation/index_delg \
+  --query_aggregation_dir data/paris6k_aggregation/query_delg \
+  --output_dir results/paris6k_aggregation_local
+  --cache_path caches/paris6k_r50/nn_inds.pkl
