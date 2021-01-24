7f377a6ee9c099d17fed0e249e305bcfc1786fe1
FUWEN TAN <fuwen.tan@gmail.com>
2021-01-24 11:44:22 -0500

    oxford

--- research/delf/delf/python/detect_to_retrieve/delg_aggregation_extraction.sh
+++ research/delf/delf/python/detect_to_retrieve/delg_aggregation_extraction.sh
@@ -1,22 +1,22 @@
 # From models/research/delf/delf/python/detect_to_retrieve
-# python extract_aggregation_delg.py \
-#   --use_query_images True \
-#   --aggregation_config_path query_aggregation_config_delg.pbtxt \
-#   --dataset_file_path data/oxford5k/gnd_roxford5k.mat \
-#   --features_dir data/oxford5k/delg_features_r50/query \
-#   --output_aggregation_dir data/oxford5k_aggregation/query_delg
-
-
 python extract_aggregation_delg.py \
   --use_query_images True \
   --aggregation_config_path query_aggregation_config_delg.pbtxt \
-  --dataset_file_path data/paris6k/gnd_rparis6k.mat \
-  --features_dir data/paris6k/delg_features_r50 \
-  --output_aggregation_dir data/paris6k_aggregation/query_delg 
+  --dataset_file_path data/oxford5k/gnd_roxford5k.mat \
+  --features_dir data/oxford5k/delg_features_r50 \
+  --output_aggregation_dir data/oxford5k_aggregation/query_delg
 
 
 # python extract_aggregation_delg.py \
-#   --aggregation_config_path index_aggregation_config_delg.pbtxt \
+#   --use_query_images True \
+#   --aggregation_config_path query_aggregation_config_delg.pbtxt \
+#   --dataset_file_path data/paris6k/gnd_rparis6k.mat \
+#   --features_dir data/paris6k/delg_features_r50 \
+#   --output_aggregation_dir data/paris6k_aggregation/query_delg 
+
+
+# python extract_aggregation_delg.py \
+#   --aggregation_config_path query_aggregation_config_delg.pbtxt \
 #   --dataset_file_path data/paris6k/gnd_rparis6k.mat \
 #   --features_dir data/paris6k/delg_features_r50 \
 #   --output_aggregation_dir data/paris6k_aggregation/index_delg 
